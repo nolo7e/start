ca3147d049215817ed673b4e216acc755e3ea9b1
nolo7e <44521170+nolo7e@users.noreply.github.com>
2020-08-15 19:34:36 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+TOKEN = 'NzQ0MjA5ODE5NzQ5MTg3NTk0.Xzf5Zw.-iLO5W_mU1ReAUtV0QWOziLgoPg'
+POST_ID = 744213838248935485  # ID ПОСТА,С КОТОРОГО СЧИТЫВАЮТСЯ РОЛИ
+ROLES = {
+    '✅': 744213085539270746,  # follower
+}  # РОЛИ
